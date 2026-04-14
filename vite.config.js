2dcf2a89b9075d03af2052756005d4613ba8ea6c
Ata00422 <167747030+Ata00422@users.noreply.github.com>
2026-04-14 09:01:10 +0500

    14 April Push

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { visualizer } from "rollup-plugin-visualizer";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    visualizer({
-      open: true
-    })
-  ],
-  build: {
-    chunkSizeWarningLimit: 500,
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173
   }
-});
+})
